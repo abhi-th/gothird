2f4564db74d96a0e5db8cd24d95f321e7de55e2b
Abhijit <abhijitthapa09@gmail.com>
2018-07-26 19:09:42 +0530

    loops finished

--- main.go
+++ main.go
@@ -1,7 +1,48 @@
+//hello world
+//package main
+//
+//import "fmt"
+//
+//func main() {
+//	fmt.Println("42")
+//}
+//print numbers
+//package main
+//
+//import "fmt"
+//
+//func main() {
+//	fmt.Println(42)
+//}
+//Print values in different numerical forms
+//package main
+//
+//import "fmt"
+//
+//func main() {
+//	fmt.Printf("%d - %b", 42, 42) prints numeber in decimal and binary form
+//	fmt.Printf("%x -%X - %o \n", 42,42,42)  %X   for bigger hex letter
+//	fmt.Printf("%d - %b \n", 42, 42) \n   for next line
+//	fmt.Println("%d \t - %b \t", 42, 42)  nothing happens \t gives tab space
+//	fmt.Print("%d - %b", 42, 42)nothing   happens shown same as here
+//}
+// loops
 package main
 
 import "fmt"
 
 func main() {
-	fmt.Println("For the purpose of git push/pull")
+	//get the sum
+//	sum := 0
+//	for i := 0; i < 20; i++ {
+//		sum += i
+//	}
+//	fmt.Println(sum)
+	//simple loops
+//	for i:=0; i< 200; i++{
+//		fmt.Printf("%d \t %b \t %#x \t %#o \n",i,i,i,i)
+//	}
+	for i:=50;i<140;i++{
+		fmt.Printf("%d \t %b \t %x \t %o %q \n",i,i,i,i,i)
+	}
 }
